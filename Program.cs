bceee01de121e5e55953e48f50e1ff0581a76966
unknown <evg.olsen@gmail.com>
2022-04-06 17:27:37 +0300

    рещил задачу

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿string[] array = { "Jonh", "Bob", "Katrin", "Anastasia" }; // Создаем массив строк
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3) count++;
+    System.Console.WriteLine(array[i]);
+}
+System.Console.WriteLine();
+
+string[] arrays = new string[count];
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        arrays[j] = array[i];
+        j++;
+    }
+}
+for (int i = 0; i < arrays.Length; i++)
+{
+    System.Console.WriteLine(arrays[i]);
+}
